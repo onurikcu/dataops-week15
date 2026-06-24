774d67cb1d03f5b344ffbb3729bde74367b2b4ef
onurikcu <onurikcu@gmail.com>
2026-06-24 22:57:17 +0300

    DAG kodu guncellendi

--- airflow3/dags/data_pipeline_dag.py
+++ airflow3/dags/data_pipeline_dag.py
@@ -1,12 +1,22 @@
 from airflow import DAG
-from airflow.providers.ssh.operators.ssh import SSHOperator
-from datetime import datetime
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
 
-with DAG('data_pipeline_assignment', start_date=datetime(2026, 6, 24), schedule_interval=None, catchup=False) as dag:
-    
-    clean_task = SSHOperator(
-        task_id='run_spark_script',
-        ssh_conn_id='ssh_default', # Airflow Connections'ta bu ID ile tanımlayacağız
-        command='python3 /opt/airflow/scripts/clean_data.py /dataops-bronze/raw/dirty_store_transactions.csv',
-        dag=dag
+default_args = {
+    'owner': 'airflow',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    'data_pipeline_assignment',
+    default_args=default_args,
+    start_date=datetime(2026, 1, 1), # Bu tarihi 2026'nın başına çektik
+    schedule_interval=None,
+    catchup=False
+) as dag:
+
+    t1 = BashOperator(
+        task_id='test_task',
+        bash_command='echo "DAG calisti!"'
     )
