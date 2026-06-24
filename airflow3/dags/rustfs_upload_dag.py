f200d419d9794863e6ef7b36672731e894da0353
onurikcu <onurikcu@gmail.com>
2026-06-25 02:19:35 +0300

    Klasör yapısı VS Code ile aynı olacak şekilde kök dizine alındı

--- airflow3/dags/rustfs_upload_dag.py
+++ airflow3/dags/rustfs_upload_dag.py
@@ -1,29 +1,35 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.standard.operators.trigger_dagrun import TriggerDagRunOperator
 from datetime import datetime
 import boto3
+import requests
+import io
 
 # Sabitler
+URL = 'https://raw.githubusercontent.com/erkansirin78/datasets/refs/heads/master/dirty_store_transactions.csv'
 BUCKET_NAME = 'dataops-bronze'
 FILE_KEY = 'raw/dirty_store_transactions.csv'
-LOCAL_FILE_PATH = '/opt/airflow/dags/dirty_store_transactions.csv'
 
-def upload_to_rustfs():
+def upload_from_url_to_rustfs():
+    # 1. GitHub'dan veriyi indir
+    response = requests.get(URL)
+    response.raise_for_status()
+    data = response.content
+    
+    # 2. RustFS'e yükle
     s3 = boto3.client('s3', 
                       endpoint_url='http://rustfs:9000', 
                       aws_access_key_id='dataops', 
                       aws_secret_access_key='Ankara06')
     
-    # Bucket oluştur
     try:
         s3.create_bucket(Bucket=BUCKET_NAME)
-    except Exception as e:
-        print(f"Bucket zaten var veya hata: {e}")
+    except:
+        pass
 
-    # Dosyayı yükle
-    s3.upload_file(LOCAL_FILE_PATH, BUCKET_NAME, FILE_KEY)
-    print(f"Veri {BUCKET_NAME} bucket'ına {FILE_KEY} yoluyla yüklendi.")
+    s3.upload_fileobj(io.BytesIO(data), BUCKET_NAME, FILE_KEY)
+    print("Veri GitHub'dan çekildi ve RustFS'e başarıyla yüklendi!")
 
 with DAG(
     'rustfs_upload_dag',
@@ -33,8 +39,8 @@ with DAG(
 ) as dag:
 
     upload_task = PythonOperator(
-        task_id='upload_to_rustfs',
-        python_callable=upload_to_rustfs
+        task_id='upload_from_url',
+        python_callable=upload_from_url_to_rustfs
     )
 
     trigger_cleaning = TriggerDagRunOperator(
